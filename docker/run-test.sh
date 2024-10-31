b84d3651c9beb9cff09e9ecebfd84ae1241079d9
Aki Mimoto <aki@zaber.com>
2024-10-31 11:26:25 -0700

    Need to ensure pdm has installed all the modules just in case

--- docker/run-test.sh
+++ docker/run-test.sh
@@ -3,6 +3,9 @@
 # Go into the project's source dir
 cd /src
 
+# Make sure we have the required modules installed
+pdm install -d
+
 # Wait till our nexus server is up and running
 # before attempting any tests
 pdm run python ./docker/wait-for-nexus.py
