224db36878d975f658a0873365e9e9d46890527b
Aki Mimoto <aki@zaber.com>
2024-11-04 07:43:29 -0800

    Use the whl package for testing

--- noxfile.py
+++ noxfile.py
@@ -1,12 +1,28 @@
 import nox
+import os
+import glob
 
 @nox.session()
 def build(session):
-    session.run("pdm", "build", )
+    session.run("pdm", "build")
 
 @nox.session(python=['/opt/pypy3/bin/pypy3', '3.8', '3.9', '3.10', '3.11', '3.12', '3.13' ])
 def tests(session):
     session.install("pytest")
-    session.install(".")
 
+    # Find the whl packages. We're going to sort by newest to oldest
+    # so for testing we can install the freshest copy
+    package_files = sorted(
+                        glob.glob("dist/izaber_wamp_zerp-*.whl"),
+                        key=os.path.getmtime,
+                        reverse=True
+                    )
+
+    if not package_files:
+        raise FileNotFoundError("No izaber_wamp_zerp-VERSION.whl package found in the 'dist' directory.")
+
+    # Install the built package
+    session.install(package_files[0])
+
+    # Run tests
     session.run("pytest", "--log-cli-level=WARN", "-s")
--- tests/test_01_import.py
+++ tests/test_01_import.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
+import sys
+
 def test_import():
+    print("\nWe are using python version:", sys.version)
     try:
         import izaber
         import izaber.wamp.zerp
